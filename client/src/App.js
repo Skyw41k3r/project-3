bb14239c23fb743b5c67ca043e0a5c1102fe91df
Billy Pinkerton <billybattlebones@gmail.com>
2022-08-13 11:49:46 -0700

    fixing errors

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,30 @@
 import React from 'react';
-import {ApolloClient, InMemoryCache, ApolloProvider,gql} from '@apollo/client'
-import {BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, } from 'react-router-dom';
+import {ApolloClient, InMemoryCache, ApolloProvider,gql} from '@apollo/client';
 import Home from './pages/Home';
 import Contact from './pages/Contact';
 import Blog from './pages/Blog';
 import About from './pages/About';
+import NavBar from './components/NavBar';
 
-//add apollo
+
+
+function App() {
+    return (
+      <ApolloProvider client={ApolloClient}>
+      <Router>
+        <>
+        <NavBar />
+        <Routes>
+          <Route exact path='/' component={Home} />
+          <Route exact path='/contact' component={Contact} />
+          <Route exact path='/blog' component={Blog} />
+          <Route render={() => <h1 className='display-2'>Wrong page!</h1>} />
+        </Routes>
+        </>
+      </Router> 
+      </ApolloProvider>
+    );
+  }
+  
+  export default App;

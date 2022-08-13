34b079ef1f2b91bc747e7fdfafabb14f2273f947
Billy Pinkerton <billybattlebones@gmail.com>
2022-08-13 10:04:03 -0700

    replacing restful api with router

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,11 @@
 import React from 'react';
-import HomePage from './components/HomePage';
+import {ApolloClient, InMemoryCache, ApolloProvider,gql} from '@apollo/client'
+import {BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Contact from './pages/Contact';
+import Blog from './pages/Blog';
+import About from './pages/About';
 
-//should be render Home with tabs
-const App = () => <HomePage/>;
+//add apollo
 
-export default App;
+import 

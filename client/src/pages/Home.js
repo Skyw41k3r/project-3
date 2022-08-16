b3eac94e7b593933109f86a75066b20631937086
Billy Pinkerton <billybattlebones@gmail.com>
2022-08-16 11:26:34 -0700

    added images to home page

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,11 +1,45 @@
 import React from "react";
 import { Card , Row } from 'antd';
 import 'antd/dist/antd.css';
+import pixelOrc from '../images/pixel-orc.png';
+import pixelSkull from '../images/pixel-skull.png';
+import pixelDonut from '../images/pixel-donut.png';
+
 
 const Home = () => (
-   <div>
-    <h1>This is the homepage</h1>
-   </div>
+   <Row justify="center">
+      <div>
+      <Card
+      hoverable
+      style={{
+         width: 240,
+      }}
+      cover={<img alt="example" src={pixelOrc} />}
+      >
+      Heres where we can include our database info
+      </Card>
+
+      <Card
+      hoverable
+      style={{
+         width: 240,
+      }}
+      cover={<img alt="example" src={pixelSkull} />}
+      > 
+      Heres where we can include our database info
+      </Card>
+
+      <Card
+      hoverable
+      style={{
+         width: 240,
+      }}
+      cover={<img alt="example" src={pixelDonut} />}
+      >
+      Heres where we can include our database info
+      </Card>
+      </div>
+   </Row>
 );
 
 export default Home;

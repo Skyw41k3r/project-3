97cac5771b1001e2ade2fbef6f96474dd77abda0
Leandro <machadoleandro98@gmail.com>
2022-08-18 15:50:44 -0700

    blog

--- client/src/pages/Blog.js
+++ client/src/pages/Blog.js
@@ -1,32 +1,59 @@
-import React from "react";
-import 'antd/dist/antd.css';
-import { Input, Form, Upload, Button } from 'antd';
-import { PlusOutlined } from '@ant-design/icons';
+import { DislikeFilled, DislikeOutlined, LikeFilled, LikeOutlined } from '@ant-design/icons';
+import { Avatar, Comment, Tooltip } from 'antd';
+import moment from 'moment';
+import React, { createElement, useState } from 'react';
 
+const App = () => {
+  const [likes, setLikes] = useState(0);
+  const [dislikes, setDislikes] = useState(0);
+  const [action, setAction] = useState(null);
+  const like = () => {
+    setLikes(1);
+    setDislikes(0);
+    setAction('liked');
+  };
 
-const Blog = () => {
+  const dislike = () => {
+    setLikes(0);
+    setDislikes(-1);
+    setAction('disliked');
+  };
+
+  const actions = [
+    <Tooltip key="comment-basic-like" title="Like">
+      <span onClick={like}>
+        {createElement(action === 'liked' ? LikeFilled : LikeOutlined)}
+        <span className="comment-action">{likes}</span>
+      </span>
+    </Tooltip>,
+    <Tooltip key="comment-basic-dislike" title="Dislike">
+      <span onClick={dislike}>
+        {React.createElement(action === 'disliked' ? DislikeFilled : DislikeOutlined)}
+        <span className="comment-action">{dislikes}</span>
+      </span>
+    </Tooltip>,
+    <span key="comment-basic-reply-to">Reply to</span>,
+  ];
   return (
     <div>
-                <Form.Item label="Upload" valuePropName="fileList">
-          <Upload action="/upload.do" listType="picture-card">
-            <div>
-              /*<PlusOutlined />*/
-              <div
-                style={{
-                  marginTop: 8,
-                }}
-              >
-                Upload
-              </div>
-            </div>
-          </Upload>
-        </Form.Item>
-        <Form.Item label="Button">
-          <Button>Submit</Button>
-        </Form.Item>
-
-
+    <Comment
+      actions={actions}
+      author={<a>Leandro Machado</a>}
+      avatar={<Avatar src="https://joeschmoe.io/api/v1/random" alt="Han Solo" />}
+      content={
+        <p>
+          I really like Cole's art piece! However, it seems they are out, when will it be in stock again? 
+        </p>
+      }
+      datetime={
+        <Tooltip title={moment().format('YYYY-MM-DD HH:mm:ss')}>
+          <span>{moment().fromNow()}</span>
+        </Tooltip>
+      }
+    />
     </div>
-)};
-
-export default Blog;
+    
+  );
+  
+};
+export default App;
